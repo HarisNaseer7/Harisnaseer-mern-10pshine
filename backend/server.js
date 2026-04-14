fb24357f7af9394de99e485e15eb1cb7524da64f
HarisNaseer7 <harisnaseer365@gmail.com>
2026-04-14 15:54:05 +0500

    completed user signup business logic

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+const app = express()
+app.get("/",(req,res)=>{
+    res.status(200).json({message:"Hello World"})
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
